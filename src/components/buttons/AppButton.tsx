9f4e74400f7bbb4be5210227a569a8ad904b5a38
Anijith <akhilanijith7056.com>
2026-02-05 10:39:49 +0530

    sigin page design and signup page

--- src/components/buttons/AppButton.tsx
+++ src/components/buttons/AppButton.tsx
@@ -1,30 +1,61 @@
-import { View, Text, TouchableOpacity } from 'react-native'
-import React from 'react'
-import { s, vs } from 'react-native-size-matters'
+import { View, Text, TouchableOpacity, StyleSheet, TextStyle } from "react-native";
+import React from "react";
+import { s, vs } from "react-native-size-matters";
+import AppText from "../texts/AppText";
+import { AppColors } from "../../styles/colors";
+interface AppButtonProps {
+    onPress: () => void;
+    title: string;
+    backgroundColor?: string;
+    textColor?: string;
+    style?: TextStyle;
+    styleTitle?: TextStyle;
+    disabled?: boolean;
 
-const AppButton = ({onPress}) => {
-  return (
-    <TouchableOpacity
-    onPress={onPress}
-    activeOpacity={0.8}
-    style={[
-        styles.button
-    ]}
-    >
-
-    </TouchableOpacity>  )
 }
 
-export default AppButton
+const AppButton : React.FC<AppButtonProps>=({ 
+  onPress,
+  title,
+  backgroundColor = AppColors.primary,
+  textColor = AppColors.white,
+  style,
+  styleTitle,
+  disabled = false,
+}) => {
+  return (
+    <TouchableOpacity
+      onPress={onPress}
+      activeOpacity={0.8}
+      style={[
+        styles.button,
+        {
+          backgroundColor: disabled ? AppColors.disabledGray : backgroundColor,
+        },
+        style
+      ]}
+      disabled={disabled}
+    >
+      <AppText varient="bold" style={[styles.textTitle, { color: textColor }]}>
+        {title}
+      </AppText>
+    </TouchableOpacity>
+  );
+};
+
+export default AppButton;
 
 const styles = StyleSheet.create({
-    button:{
-        width: "100%",
-        height: vs(40),
-        backgroundColor: "blue",
-        borderRadius: s(25),
-        justifyContent: "center",
-        alignItems: "center",
-        aligself:"center"
-    }
-})
+  button: {
+    width: "100%",
+    height: vs(40),
+    backgroundColor: "blue",
+    borderRadius: s(25),
+    justifyContent: "center",
+    alignItems: "center",
+    alignSelf: "center",
+  },
+  textTitle: {
+    fontSize: s(16),
+  },
+});
---
+++ src/screens/auth/SignInScreen.tsx
@@ -0,0 +1,53 @@
+import { StyleSheet, Text, Image } from "react-native";
+import React, { useState } from "react";
+import AppSaveView from "../../components/views/AppSaveView";
+import { sharedPaddingHorzizontal } from "../../styles/sharedStyles";
+import { IMAGES } from "../../constants/images-paths";
+import { s, vs } from "react-native-size-matters";
+import AppTextInput from "../../components/inputs/AppTextInput";
+import AppText from "../../components/texts/AppText";
+import AppButton from "../../components/buttons/AppButton";
+import { AppColors } from "../../styles/colors";
+
+const SignInScreen = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  return (
+    <AppSaveView style={styles.container}>
+      <Image source={IMAGES.appLogo} style={styles.logo} />
+      <AppTextInput placeholder="Email" onChangeText={setEmail} />
+      <AppTextInput
+        placeholder="Password"
+        onChangeText={setPassword}
+        secureTextEntry
+      />
+      <AppText style={styles.appName}>Smart E-commerce</AppText>
+      <AppButton title="Login" />
+      <AppButton title="Sign up" style={styles.registerButton} textColor={AppColors.primary} />
+    </AppSaveView>
+  );
+};
+
+export default SignInScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: "center",
+    paddingHorizontal: sharedPaddingHorzizontal,
+  },
+  logo: {
+    height: s(150),
+    width: s(150),
+    marginBottom: vs(30),
+  },
+  appName: {
+    fontSize: s(16),
+    marginBottom: vs(15),
+  },
+  registerButton:{
+    backgroundColor:AppColors.white,
+    borderWidth:1,
+    marginTop:vs(15),
+    borderColor:AppColors.primary,
+  }
+});
